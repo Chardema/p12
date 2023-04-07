351a86fe979926655f64eb9649c6adb90f42a1c9
HARDEMAN CHRISTOPHER <41175936+Chardema@users.noreply.github.com>
2023-04-07 15:49:37 +0200

    Maj header et LeftBar 2

--- src/components/Leftbar/Leftbar.js
+++ src/components/Leftbar/Leftbar.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import styles from './Leftbar.module.scss';
+import swimmingIcon from './../../img/swimming-icon.svg'
+import yogaIcon from './../../img/yoga-icon.svg'
+import bikeIcon from './../../img/bike-icon.svg'
+import dumbbellIcon from './../../img/dumbbell-icon.svg'
 
 const Leftbar = () => {
     return (
         <div className={styles.sidebar}>
-            <div className={styles.square}></div>
-            <div className={styles.square}></div>
-            <div className={styles.square}></div>
-            <div className={styles.square}></div>
+            <div className={styles.square}>
+                <img src={yogaIcon} alt="Swimming icon" className={styles.icon} />
+            </div>
+            <div className={styles.square}>
+                <img src={swimmingIcon} alt="Swimming icon" className={styles.icon} />
+            </div>
+            <div className={styles.square}>
+                <img src={bikeIcon} alt="Swimming icon" className={styles.icon} />
+            </div>
+            <div className={styles.square}>
+                <img src={dumbbellIcon} alt="Swimming icon" className={styles.icon} />
+            </div>
             <div className={styles.copy}>Copiryght, SportSee 2020</div>
         </div>
     );
